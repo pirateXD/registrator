315d36cbd4d9d23da808f833f750a346e7e13517
yangjunhai <yangjunhai@xindong.com>
2019-05-30 12:22:55 +0800

    registrator 1.0.2 build shell

--- docker_builder.sh
+++ docker_builder.sh
@@ -7,6 +7,10 @@ if [[ -z "$tag" ]]; then
     tag="latest"
 fi
 echo "tag is: $tag"
+# export GOPATH=$(cd ../../..; pwd)
+echo "GOPATH : $GOPATH"
+export GOOS=linux
+export GOARCH=amd64
 
 docker build -t ${registryHost}registrator:${tag} -f Dockerfile .
 docker push ${registryHost}registrator:${tag}

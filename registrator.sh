1578c9f2a8c77884dd4174fe7f7223fadbcb8718
yangjunhai <yangjunhai@xindong.com>
2019-07-10 19:23:10 +0800

    修改 etcd host url

--- registrator.sh
+++ registrator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ip=172.26.147.228
+ip=172.26.148.189
 tag=$(grep "^version" docker_run.config | awk -F'=' '{print $2}')
 id=myRegistrator
 dockerRun=qa.haidao:5000/registrator:${tag}
@@ -9,7 +9,7 @@ docker stop $id && docker rm $id && echo "stop and remove $id"
 docker run -d  --name=$id  --net=host  --volume=/var/run/docker.sock:/tmp/docker.sock  $dockerRun  \
 -ip="$ip" \
 -useIpFromLabel="exposeIP" \
--ttl=30 \
--ttl-refresh=10  \
+-ttl=60 \
+-ttl-refresh=30  \
 -event-channel-len=1024 \
 etcd://qa.haidao:2379/services
